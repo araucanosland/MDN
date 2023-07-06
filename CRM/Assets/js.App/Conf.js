5d58d42ed212fd9dc339934879aac8cd8d0e2776
Victor Arroyo <victor.arroyo.n@gmail.com>
2023-07-06 14:31:01 -0400

    ultimo commit

--- CRM/Assets/js.App/Conf.js
+++ CRM/Assets/js.App/Conf.js
@@ -1,4 +1,4 @@
 ﻿var BASE_URL = "http://localhost:49969";
 var DEBUG_JS = true;
 var LEAD_SERVICE_BASE_URL = "https://localhost:5001";
-const motor_api_server = 'serv-297';
+const motor_api_server = 'serv-292';
